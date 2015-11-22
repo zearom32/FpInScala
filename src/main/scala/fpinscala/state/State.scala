c312df40e9396446d9550d4af894f0468797eff1
zearom32 <zearom32@gmail.com>
2015-11-22 23:57:04 +0800

    add State. Difficult to understand. (sad).

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -108,6 +108,7 @@ object RNG{
     g(a)(r1)
   }
 
+  //Ex10. Reimplement map and map2 interms of flapMap
   def _map[A,B](s:Rand[A])(f: A => B):Rand[B] =
   {
     flatMap(s)(a => unit(f(a)))
@@ -116,6 +117,52 @@ object RNG{
   def _map2[A,B,C](ra:Rand[A], rb:Rand[B])(f: (A,B) => C):Rand[C] =  {
     flatMap(ra)(a => map(rb)(b => f(a,b)))
   }
+
+
+}
+
+case class State[S,+A](run: S => (A,S)){
+  //Ex11. Generalize the functions unit, map, map2, flatMap and sequence. Add them as methods on the State case class where possible. Otherwise you should put them in a State companion object.
+  def map[B](f: A => B): State[S,B] = flatMap(a => State.unit(f(a)))
+
+  //notice here.
+  //nice implementation.
+  def map2[B,C](sb:State[S,B])(f:(A,B) => C):State[S,C] = flatMap(a => sb.map(b => f(a,b)))
+
+  def flatMap[B](f: A => State[S,B]):State[S,B] = State(s =>{
+    val (i,r) = run(s)
+    f(i).run(r)
+  })
+
+}
+
+object State{
+  type Rand[A] = State[RNG,A]
+
+  def unit[S,A](a:A):State[S,A] = State(s => (a,s))
+
+  def sequence[S,A](sas: List[State[S,A]]):State[S,List[A]] = {
+    def go(s:S, actions:List[State[S,A]],acc:List[A]):(List[A],S) = actions match{
+        case Nil => (acc.reverse, s)
+        case h::t => h.run(s) match {case (a,s2) => go(s2,t,a::acc)}
+      }
+    State((s:S) => go(s,sas,List()))
+  }
+
+
+  def sequence2[S,A](sas: List[State[S,A]]):State[S,List[A]] = {
+    sas.foldRight(unit[S,List[A]](List[A]()))((a,acc) => a.map2(acc)(_ :: _))
+  }
+
+
+  def modify[S](f: S => S):State[S,Unit] = for {
+    s <- get
+    _ <- set(f(s))
+   } yield ()
+
+  def get[S]:State[S,S] = State(s => (s,s))
+  def set[S](s:S):State[S,Unit] = State(_ => ((),s))
+
 
 }
 
